d1479ac8dc9d22e49d91ac704e839376d5da8f23
ABHISHEK SAPATE <136620220+ABHISHEKSAPATE@users.noreply.github.com>
2024-11-21 01:36:55 +0530

    Add files via upload

---
+++ BASICDEMO1/PALINDROMEORNOT/PALINDROMEORNOT/Palindromeornot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PALINDROMEORNOT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 119, rev = 0;
+            for (int i = num; i != 0; i = i / 10)
+            {
+                int digit = i % 10;
+                rev = rev * 10 + digit;
+            }
+            if (num == rev)
+            {
+                Console.WriteLine("Number is Palindrome ");
+            }
+            else
+            {
+                Console.WriteLine("Number is Not Palindrome ");
+            }
+        }
+    }
+}
---
+++ BASICDEMO1/PATTERNPROGRAM/PATTERNPROGRAM/Patternprogram.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PATTERNPROGRAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // //1)
+            // //*****
+            // // ****
+            // //  ***
+            // //   **
+            // //    *
+
+            //for(int i = 5; i>=1; i--)
+            //{
+            //    for(int j=i;j>=1;j--)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //    8)     WAP to print following Pattern
+            //1 0 1 0 1
+            //0 1 0 1 0
+            //1 0 1 0 1
+            //0 1 0 1 0
+            //1 0 1 0 1
+
+            //    int n = 5;
+            //    for(int i=1;i<=n;i++)
+            //    {
+            //        if(i%2==0)
+            //        {
+            //            Console.WriteLine("10101");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("01010");
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+            //2) Print pattern
+            //1
+            //12
+            //123
+            //1234
+            //12345
+            //1234
+            //123
+            //12
+            //1
+
+
+            //int n = 5;
+            //for (int i = 1; i <=n; i++)//first half
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+            //for (int i = n-1; i >=1; i--)//second half
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+
+
+            // 6.WAP to print following Pattern
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //*****
+            //****
+            //***
+            //**
+            //*.
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)//first half
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+            //for (int i = n - 1; i >= 1; i--)//second half
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+
+
+            //5)
+            //      1
+            //     2 2
+            //    3 3 3
+            //   4 4 4 4
+            //  5 5 5 5 5
+            // 6 6 6 6 6 6
+            //7 7 7 7 7 7 7
+
+
+            //int n = 7; // Total number of rows
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    // Print spaces
+            //    for (int j = i; j < n; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(i +" ");
+            //    }
+
+            //    // Move to the next line
+            //    Console.WriteLine();
+            //}
+
+            //4)
+
+            //      1
+            //     1 2
+            //    1 2 3
+            //   1 2 3 4
+            //  1 2 3 4 5
+            // 1 2 3 4 5 6
+            //1 2 3 4 5 6 7
+
+            //int n = 7; // Total number of rows
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    // Print spaces
+            //    for (int j = i; j < n; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(k + " ");
+            //    }
+
+            //    // Move to the next line
+            //    Console.WriteLine();
+            //}
+
+            //7) WAP to print following Pattern
+            //    *
+            //   * *
+            //  * * *
+            // * * * *
+            //* * * * *
+            // * * * *
+            //  * * *
+            //   * *
+
+            //int n = 7; // Total number of rows
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    // Print spaces
+            //    for (int j = i; j < n; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*" + " ");
+            //    }
+
+            //    // Move to the next line
+            //    Console.WriteLine();
+            //}
+            //for (int i = n-1; i >= 1; i--)
+            //{
+            //    // Print spaces
+            //    for (int j = i; j < n; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*" + " ");
+            //    }
+
+            //    // Move to the next line
+            //    Console.WriteLine();
+            //}
+
+
+
+
+
+        }
+    }
+}
